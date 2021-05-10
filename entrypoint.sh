fbed2f768fc9ad627b0f76746ea104d61e40889e
Greg Adams <greg@moderne.io>
2021-05-10 15:23:28 -0700

    add all the repos

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 
-printf '%s' "$INPUT_GCP_SERVICE_ACCOUNT_KEY" > /tmp/gcp_service_account_key.json
+printf '%s' "${INPUT_GCP-SERVICE-ACCOUNT-KEY}" > /tmp/gcp_service_account_key.json
 export GOOGLE_APPLICATION_CREDENTIALS="/tmp/gcp_service_account_key.json"
 
 cd $GITHUB_WORKSPACE/main
