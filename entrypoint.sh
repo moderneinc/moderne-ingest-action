f871d2b964b5bcb552e07e82b51bd1f000f1b595
Greg Adams <greg@moderne.io>
2021-05-10 12:19:14 -0700

    experimental

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-echo $INPUT_GCP_SERVICE_ACCOUNT_KEY > /tmp/gcp_service_account_key.json
+printf '%s' "$INPUT_GCP_SERVICE_ACCOUNT_KEY" > /tmp/gcp_service_account_key.json
 cat /tmp/gcp_service_account_key.json
 
 /usr/local/gcloud/google-cloud-sdk/bin/gcloud --quiet auth activate-service-account --key-file=/tmp/gcp_service_account_key.json
