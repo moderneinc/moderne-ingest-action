0e1793a0c045a09d529e8cc7bc6bb88c7487b4d3
Greg Adams <greg@moderne.io>
2021-05-10 15:28:53 -0700

    troubleshooting

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 
 printf '%s' "${INPUT_GCP-SERVICE-ACCOUNT-KEY}" > /tmp/gcp_service_account_key.json
+cat /tmp/gcp_service_account_key.json
 export GOOGLE_APPLICATION_CREDENTIALS="/tmp/gcp_service_account_key.json"
 
 cd $GITHUB_WORKSPACE/main
