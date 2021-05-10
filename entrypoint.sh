c126bd8dc6d5df7760e21c275bd42dc872dc26da
Greg Adams <greg@moderne.io>
2021-05-10 15:33:55 -0700

    troubleshooting

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/sh -l
 
-
-printf '%s' "${INPUT_GCP-SERVICE-ACCOUNT-KEY}" > /tmp/gcp_service_account_key.json
-cat /tmp/gcp_service_account_key.json
-export GOOGLE_APPLICATION_CREDENTIALS="/tmp/gcp_service_account_key.json"
+printf '%s' "${INPUT_GCPKEY}" > /tmp/gcp_key.json
+export GOOGLE_APPLICATION_CREDENTIALS="/tmp/gcp_key.json"
 
 cd $GITHUB_WORKSPACE/main
 ./gradlew --no-daemon bootRun --args='--spring.profiles.active=prod'
