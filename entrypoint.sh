6a9aa5e5e9ed5d54765db8ef6735726e98959725
Greg Adams <greg@moderne.io>
2021-05-10 12:29:23 -0700

    experimental

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh -l
 
-printf '%s' "$INPUT_GCP_SERVICE_ACCOUNT_KEY" > /tmp/gcp_service_account_key.json
-cat /tmp/gcp_service_account_key.json
 
-/usr/local/gcloud/google-cloud-sdk/bin/gcloud --quiet auth activate-service-account --key-file=/tmp/gcp_service_account_key.json
+printf '%s' "$INPUT_GCP_SERVICE_ACCOUNT_KEY" > /tmp/gcp_service_account_key.json
+export GOOGLE_APPLICATION_CREDENTIALS="/tmp/gcp_service_account_key.json"
+
+# /usr/local/gcloud/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file=/tmp/gcp_service_account_key.json
 
 cd $GITHUB_WORKSPACE/main
 ./gradlew --no-daemon bootRun --args='--spring.profiles.active=prod'
