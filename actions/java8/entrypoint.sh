f53ddae2040693e812e93792db418c0eb5f5e351
Greg Adams <greg@moderne.io>
2021-05-17 09:50:47 -0700

    use styleName

--- actions/java8/entrypoint.sh
+++ actions/java8/entrypoint.sh
@@ -4,4 +4,4 @@ printf '%s' "${INPUT_GCPKEY}" > /tmp/gcp_key.json
 export GOOGLE_APPLICATION_CREDENTIALS="/tmp/gcp_key.json"
 
 cd $GITHUB_WORKSPACE/main
-./gradlew --no-daemon bootRun --args='--spring.profiles.active=prod'
+./gradlew --no-daemon bootRun --args='--spring.profiles.active=prod --styleName=${INPUT_STYLENAME}'
