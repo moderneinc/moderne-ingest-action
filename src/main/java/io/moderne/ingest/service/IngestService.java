e89057475e9be3a86ce14a1976784b94959f23ec
Jonathan Schneider <jkschneider@gmail.com>
2021-06-03 09:14:35 -0700

    Load styles from rewrite core JARs and not from external sources (fixes #12)

--- src/main/java/io/moderne/ingest/service/IngestService.java
+++ src/main/java/io/moderne/ingest/service/IngestService.java
@@ -4,9 +4,8 @@ import io.moderne.ingest.parse.GradleProjectParser;
 import io.moderne.ingest.parse.MavenProjectParser;
 import io.moderne.ingest.parse.ParseResult;
 import io.moderne.ingest.store.SourceFileRepository;
-import io.moderne.ingest.store.StyleRepository;
 import org.openrewrite.SourceFile;
-import org.openrewrite.config.YamlResourceLoader;
+import org.openrewrite.config.Environment;
 import org.openrewrite.internal.lang.Nullable;
 import org.openrewrite.java.style.Autodetect;
 import org.openrewrite.java.style.ImportLayoutStyle;
@@ -20,31 +19,29 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import java.io.InputStream;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
 public class IngestService {
 
     private final Logger log = LoggerFactory.getLogger(IngestService.class);
+    private static final Environment environment = Environment.builder().scanRuntimeClasspath().build();
 
     private final MavenProjectParser mavenProjectParser;
     private final GradleProjectParser gradleProjectParser;
     private final SourceFileRepository sourceFileRepository;
-    private final StyleRepository styleRepository;
 
     public IngestService(MavenProjectParser mavenProjectParser,
-                         GradleProjectParser gradleProjectParser, SourceFileRepository sourceFileRepository,
-                         StyleRepository styleRepository) {
+                         GradleProjectParser gradleProjectParser, SourceFileRepository sourceFileRepository) {
         this.mavenProjectParser = mavenProjectParser;
         this.gradleProjectParser = gradleProjectParser;
         this.sourceFileRepository = sourceFileRepository;
-        this.styleRepository = styleRepository;
     }
 
     public void ingest(Path projectDir, @Nullable String styleName) {
@@ -62,14 +59,7 @@ public class IngestService {
 
             Collection<NamedStyles> namedStyles;
             if (styleName != null) {
-                InputStream styleInputStream = styleRepository.getStyleInputStream(styleName);
-                YamlResourceLoader yamlResourceLoader;
-                try {
-                    yamlResourceLoader = new YamlResourceLoader(styleInputStream, new URI("urn:moderne-saas:" + styleName), new Properties());
-                } catch (URISyntaxException e) {
-                    throw new RuntimeException(e);
-                }
-                namedStyles = yamlResourceLoader.listStyles();
+                namedStyles = environment.activateStyles(styleName);
             } else {
                 Autodetect autodetect = Autodetect.detect(parseResult.getSourceFiles().stream()
                         .filter(J.CompilationUnit.class::isInstance)
--- src/main/java/io/moderne/ingest/store/StyleRepository.java
+++
@@ -1,9 +0,0 @@
-package io.moderne.ingest.store;
-
-import java.io.InputStream;
-import java.util.List;
-
-public interface StyleRepository {
-
-    InputStream getStyleInputStream(String styleName);
-}
